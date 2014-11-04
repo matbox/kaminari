ef786a2bf17b7cb1af67c9daebd5c7c07eefc38b
Matthias <matthias@matbox.net>
2014-11-03 19:08:42 -0800

    fix for broken merge

--- lib/kaminari/helpers/action_view_extension.rb
+++ lib/kaminari/helpers/action_view_extension.rb
@@ -15,30 +15,14 @@ module Kaminari
     # * <tt>:remote</tt> - Ajax? (false by default)
     # * <tt>:ANY_OTHER_VALUES</tt> - Any other hash key & values would be directly passed into each tag as :locals value.
     def paginate(scope, options = {}, &block)
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> FETCH_HEAD
       if @current_page
         cpage = @current_page
       else
         cpage = scope.current_page
       end
+
       paginator = Kaminari::Helpers::Paginator.new self, options.reverse_merge(:current_page => cpage, :total_pages => scope.total_pages, :per_page => scope.limit_value, :remote => false)
-<<<<<<< HEAD
-=======
       options[:total_pages] ||= scope.total_pages
-
-      paginator = Kaminari::Helpers::Paginator.new(self, options.reverse_merge(:current_page => scope.current_page, :per_page => scope.limit_value, :remote => false))
->>>>>>> FETCH_HEAD
-=======
-      options[:total_pages] ||= scope.total_pages
-
-      paginator = Kaminari::Helpers::Paginator.new(self, options.reverse_merge(:current_page => scope.current_page, :per_page => scope.limit_value, :remote => false))
->>>>>>> FETCH_HEAD
-=======
->>>>>>> FETCH_HEAD
       paginator.to_s
     end
 
